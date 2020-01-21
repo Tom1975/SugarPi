9e8d51470f7f079ad2878ae0e8ddc865505c4d54
Thomas Guillemin <thomas.guillemin@gmail.com>
2020-01-21 21:51:08 +0100

    update build for pi3/4

--- build_pi4.sh
+++ build_pi4.sh
@@ -3,7 +3,7 @@ echo "*** Build target for Raspberry Pi 3 ***"
 # Set arch
 echo AARCH = 64							> Config.mk
 echo RASPPI = 4							>> Config.mk
-echo PREFIX64 = aarch64-elf-			>> Config.mk
+echo PREFIX64 = aarch64-non-elf-		>> Config.mk
 echo STDLIB_SUPPORT = 1					>> Config.mk
 echo DEFINE = -DARM_ALLOW_MULTI_CORE	>> Config.mk
 
