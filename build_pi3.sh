b50504e6c0ce00ea7e352a89a4a5266a3c46146e
Thomas Guillemin <thomas.guillemin@gmail.com>
2020-10-20 23:31:38 +0200

    change a bit display (with coherent buffer handling). Also prepare a qemu  debug launch

--- build_pi3.sh
+++ build_pi3.sh
@@ -1,11 +1,12 @@
 echo "*** Build target for Raspberry Pi 3 ***"
 
 # Set arch
-echo AARCH = 64							> Config.mk
-echo RASPPI = 3							>> Config.mk
+echo AARCH = 64							          > Config.mk
+echo RASPPI = 3							          >> Config.mk
 echo PREFIX64 = aarch64-none-elf-   	>> Config.mk
-echo STDLIB_SUPPORT = 1					>> Config.mk
+echo STDLIB_SUPPORT = 1					      >> Config.mk
 echo DEFINE = -DARM_ALLOW_MULTI_CORE	>> Config.mk
+echo CHECK_DEPS = 0                   >> Config.mk
 
 cp Config.mk circle/.
 ./build_circle.sh
@@ -19,6 +20,9 @@ make
 if [ ! -d "sdcard" ]; then
   mkdir sdcard
 fi
-cp kernel8.img sdcard/.
+
+cp circle/boot/* sdcard/pi3
+cp kernel8.img sdcard/pi3
+
 
 echo "*** End ***"
