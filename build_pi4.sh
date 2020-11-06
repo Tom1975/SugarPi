0132009d662482b838badf36de3af24ef66d5dfc
Thomas Guillemin <thomas.guillemin@gmail.com>
2020-11-06 22:52:21 +0100

    Remove build for pi1 & 2;

--- build_pi4.sh
+++ build_pi4.sh
@@ -3,11 +3,10 @@ echo "*** Build target for Raspberry Pi 4 ***"
 # Set arch
 echo AARCH = 64							          > Config.mk
 echo RASPPI = 4							          >> Config.mk
-echo PREFIX64 = aarch64-elf-		      >> Config.mk
+echo PREFIX64 = aarch64-none-elf-     >> Config.mk
 echo STDLIB_SUPPORT = 1					      >> Config.mk
 echo DEFINE = -DARM_ALLOW_MULTI_CORE	>> Config.mk
 echo CHECK_DEPS = 0                   >> Config.mk
-echo POSTFIX = .exe 					        >> Config.mk
 
 cp Config.mk circle/.
 ./build_circle.sh
