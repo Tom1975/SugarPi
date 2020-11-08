4e5c25d2e201f75dc54bf4b2e37eb32998216295
Thomas Guillemin <thomas.guillemin@gmail.com>
2020-11-08 21:02:24 +0100

    Update docker & actions

--- sugarpi-action/entrypoint_docker.sh
+++ sugarpi-action/entrypoint_docker.sh
@@ -1,11 +1,12 @@
 #!/bin/sh -l
 echo "PATH : $PATH"
+echo $(pwd)
 echo "who-to-build value : $1"
 ls 
 
-chmod +x /build_all.sh
-chmod +x /build_circle.sh
-chmod +x /build_pi3.sh
-chmod +x /build_pi4.sh
+chmod +x ./build_all.sh
+chmod +x ./build_circle.sh
+chmod +x ./build_pi3.sh
+chmod +x ./build_pi4.sh
 
 ./build_all.sh
