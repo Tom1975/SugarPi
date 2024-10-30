0650c9f8d5dd888d4a9bdcaae6d98a1885d90549
Thomas GUILLEMIN <thomas.guillemin@conduent.com>
2024-10-30 14:33:53 +0100

    Fix build

--- src/ScreenMenu.cpp
+++ src/ScreenMenu.cpp
@@ -9,16 +9,28 @@
 
 #include "MenuButtonWithBitmapWindows.h"
 
-#define PROFILE
+#ifdef WIN32
+   #define PROFILE
 
-#ifdef PROFILE
-#include <profileapi.h>
-#define START_CHRONO  QueryPerformanceFrequency((LARGE_INTEGER*)&freq);;QueryPerformanceCounter ((LARGE_INTEGER*)&s1);
-#define STOP_CHRONO   QueryPerformanceCounter ((LARGE_INTEGER*)&s2);t=(DWORD)(((s2 - s1) * 1000000) / freq);
-#define PROF_DISPLAY sprintf(s, "Duree Chargement descriptions: %d us\n", t);OutputDebugString (s);
-static __int64 s1, s2, freq;
-static DWORD t;
-static char s[1024];
+   #ifdef PROFILE
+      #include <profileapi.h>
+      #define START_CHRONO  QueryPerformanceFrequency((LARGE_INTEGER*)&freq);;QueryPerformanceCounter ((LARGE_INTEGER*)&s1);
+      #define STOP_CHRONO   QueryPerformanceCounter ((LARGE_INTEGER*)&s2);t=(DWORD)(((s2 - s1) * 1000000) / freq);
+      #define PROF_DISPLAY sprintf(s, "Duree Chargement descriptions: %d us\n", t);OutputDebugString (s);
+      static __int64 s1, s2, freq;
+      static DWORD t;
+      static char s[1024];
+   #else
+      #define START_CHRONO  
+      #define STOP_CHRONO   
+      #define PROF_DISPLAY 
+
+   #endif
+#else
+      #define START_CHRONO  
+      #define STOP_CHRONO   
+      #define PROF_DISPLAY 
+
 #endif
 
 #ifdef  __circle__
