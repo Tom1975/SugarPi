06980b866816eddc32d0ab0a92e19efc54d0a2ea
Thomas Guillemin <thomas.guillemin@gmail.com>
2024-12-01 23:09:27 +0100

    add logs to debug key press

--- src/KeyboardPi.cpp
+++ src/KeyboardPi.cpp
@@ -307,18 +307,19 @@ KeyboardPi::~KeyboardPi()
 
 void KeyboardPi::UnpressKey(unsigned int scancode)
 {
+   logger_->Write("KeyboardPi", LogNotice, "PressKey %X - line : %i, bit : %X", scancode, handler_.raw_to_cpc_map_[scancode & 0xFF].line_number, handler_.raw_to_cpc_map_[scancode & 0xFF].bit);
    if (handler_.raw_to_cpc_map_[scancode & 0xFF].bit != 0)
    {
-      //logger_->Write("KeyboardPi", LogNotice, "PressKey %X - line : %i, bit : %X", scancode, raw_to_cpc_map_[scancode & 0xFF].line_number, raw_to_cpc_map_[scancode & 0xFF].bit);
       *handler_.raw_to_cpc_map_[scancode].line_index |= (handler_.raw_to_cpc_map_[scancode].bit);
    }
 }
 
 void KeyboardPi::PressKey(unsigned int scancode)
 {
+   logger_->Write("KeyboardPi", LogNotice, "PressKey %X - line : %i, bit : %X", scancode, handler_.raw_to_cpc_map_[scancode & 0xFF].line_number, handler_.raw_to_cpc_map_[scancode & 0xFF].bit);
    if (handler_.raw_to_cpc_map_[scancode & 0xFF].bit != 0)
    {
-      //logger_->Write("KeyboardPi", LogNotice, "UnpressKey %X - line : %i, bit : %X", scancode, raw_to_cpc_map_[scancode & 0xFF].line_number, raw_to_cpc_map_[scancode & 0xFF].bit);
+      
       *handler_.raw_to_cpc_map_[scancode & 0xFF].line_index &= ~(handler_.raw_to_cpc_map_[scancode & 0xFF].bit);
    }   
 }
